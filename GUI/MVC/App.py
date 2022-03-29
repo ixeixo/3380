1ebc9b107554e7fdc8783d93548206332093fdc3
GunnarO0 <69883423+GunnarO0@users.noreply.github.com>
2022-03-28 20:52:42 -0500

    Add files via upload

---
+++ GUI/MVC/App.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+from MVC.Model import Model
+from MVC.View import View
+from MVC.Controller import Controller
+
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Tkinter MVC Demo')
+
+        # create a model
+        model = Model('hello@pythontutorial.net')
+
+        # create a view and place it on the root window
+        view = View(self)
+        view.grid(row=0, column=0, padx=10, pady=10)
+
+        # create a controller
+        controller = Controller(model, view)
+
+        # set the controller to view
+        view.set_controller(controller)
