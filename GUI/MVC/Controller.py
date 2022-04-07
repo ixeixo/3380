d9ef77e4335a16cf7424576201ab3f8c1648bc58
GunnarO0 <69883423+GunnarO0@users.noreply.github.com>
2022-04-06 22:53:22 -0500

    Update Controller.py

--- GUI/MVC/Controller.py
+++ GUI/MVC/Controller.py
@@ -4,25 +4,5 @@ class Controller:
         self.view = view
 
 
-
-
-    # sample code
-
-    #def save(self, email):
-    #    """
-    #    Save the email
-    #    :param email:
-    #    :return:
-    #    """
-    #    try:
-
-    #        # save the model
-    #        self.model.email = email
-    #        self.model.save()
-
-    #        # show a success message
-    #        self.view.show_success(f'The email {email} saved!')
-
-    #    except ValueError as error:
-    #        # show an error message
-    #        self.view.show_error(error)
+    def SendFile(self, filename):
+        return
