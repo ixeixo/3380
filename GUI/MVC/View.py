99f6a9e546782e9f94d13921982670930dbbccd6
GunnarO0 <69883423+GunnarO0@users.noreply.github.com>
2022-04-07 15:01:26 -0500

    Update View.py
    Updated the GUI to replace the check match button with an append info button. The hashify button will now automatically be apart of the send and check match process. The lower half of the UI is to only appear if a match is not found.

--- GUI/MVC/View.py
+++ GUI/MVC/View.py
@@ -6,13 +6,11 @@ from PIL import ImageTk,Image
 from datetime import date
 from datetime import datetime
 
-fileName = "";
+fileName = ""
 
 class View(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
-
-        self.fileName = ""
 
         self.frameMain = Frame(self)
         self.frameMain.grid(row=0, column=0)
@@ -33,86 +31,93 @@ class View(ttk.Frame):
         self.uploadLabel.grid(row=1, column=1)
         # upload photo button
         self.uploadPhoto_button = ttk.Button(self.frameMain, text='Choose Photo', width=30, command=self.browseFiles)
-        self.uploadPhoto_button.grid(row=2, column=1, padx=5, sticky='w')
+        self.uploadPhoto_button.grid(row=2, column=1, padx=5)
+        
+        # hashify button
+        self.hashify_button = ttk.Button(self.frameMain, text='Hashify', width=30, command=self.hashify_button_clicked, state=DISABLED)
+        self.hashify_button.grid(row=3, column=1, padx=5)
+        # hashify status label
+        self.hashifyStatusLabel = ttk.Label(self.frameMain, text=' ')
+        self.hashifyStatusLabel.grid(row=4, column=1, padx=5, pady=5)
 
 
 
         # create empty preview canvas
-        self.previewCanvas = tk.Canvas(self.frameCanvas, bg='grey', width=550,height=350)
+        self.previewCanvas = tk.Canvas(self.frameCanvas, bg='grey', width=600,height=350)
         self.previewCanvas.grid(row=4,column=2,pady=30)
         #  create empty image
         self.displayPreviewImage = ttk.Label(self.frameCanvas)
 
 
-        # file details
-        # name
-        self.imageName = ttk.Label(self.frameDetails, text="Image Name: ")
-        self.imageName.grid(row=1, column=1, padx=5, sticky='w')
-        self.imageNameInput = tk.Text(self.frameDetails, height=1, width=25)
-        self.imageNameInput.grid(row=1, column=2, padx=5, pady=5)
-        # date
-        self.imageDate = ttk.Label(self.frameDetails, text="Date:")
-        self.imageDate.grid(row=2, column=1, padx=5, sticky='w')
-        self.imageDateLabel = ttk.Label(self.frameDetails, text=date.today().strftime("%m/%d/%y"))
-        self.imageDateLabel.grid(row=2, column=2, padx=5, pady=5, sticky='w')
-        # time
-        self.imageTime = ttk.Label(self.frameDetails, text="Time:")
-        self.imageTime.grid(row=3, column=1, padx=5, sticky='w')
-        self.imageTimeLabel = ttk.Label(self.frameDetails, text=datetime.now().strftime("%H:%M:%S"))
-        self.imageTimeLabel.grid(row=3, column=2, padx=5, pady=5, sticky='w')
-        # description
-        self.imageDescription = ttk.Label(self.frameDetails, text="Description:")
-        self.imageDescription.grid(row=4, column=1, padx=5, sticky='w')
-        self.imageDescriptionInput = tk.Text(self.frameDetails, height=6, width=25)
-        self.imageDescriptionInput.grid(row=4, column=2, padx=5, pady=5)
- 
-        
-
-        # Hash and Match Buttons
-        # hashify button
-        self.hashify_button = ttk.Button(self.frameDetails, text='Hashify', width=30, command=self.hashify_button_clicked, state=DISABLED)
-        self.hashify_button.grid(row=9, column=2, padx=5)
-        # hashify status label
-        self.hashifyStatusLabel = ttk.Label(self.frameDetails, text=' ', wraplength=400)
-        self.hashifyStatusLabel.grid(row=9, column=3, padx=5, pady=5)
-
-        # check match button
-        self.checkMatch_button = ttk.Button(self.frameDetails, text='Check for Match', width=30, command=self.checkMatch_button_clicked, state=DISABLED)
-        self.checkMatch_button.grid(row=10, column=2, padx=5, pady=5)
-        # is match label
-        self.matchStatusLabel = ttk.Label(self.frameDetails, text=' ', wraplength=400)
-        self.matchStatusLabel.grid(row=10, column=3, padx=5, pady=5)
-
-
-
+         
+        # FUNCTIONS
 
         # set the controller
         self.Controller = None
     def set_controller(self, controller):
         self.Controller = controller
 
-
-         
-        # functions
-
+        # hashify button
     def hashify_button_clicked(self):
         self.Controller.SendFile(self.fileName)
-        self.hashifyStatusLabel.configure(text="Image has been hashed!")
+        # <Insert Hash Call>
+        
+        # Get matchfound result and replace the "false" below
+        if(False):
+            # Match Found
+            self.hashifyStatusLabel.configure(text="Image has been hashed. Match found!")
+        
+        else:
+            # Match Not Found
+            self.hashifyStatusLabel.configure(text="Image has been hashed. No match found, please add additional information below.")
+            self.hashify_button['state'] = DISABLED
+        
+            # ACTIVATE INFORMATION SECTION
+            # file details
+            # name
+            self.imageName = ttk.Label(self.frameDetails, text="Image Name: ")
+            self.imageName.grid(row=1, column=1, padx=5, sticky='w')
+            self.imageNameInput = tk.Text(self.frameDetails, height=1, width=25)
+            self.imageNameInput.grid(row=1, column=2, padx=5, pady=5)
+            # date
+            self.imageDate = ttk.Label(self.frameDetails, text="Date:")
+            self.imageDate.grid(row=2, column=1, padx=5, sticky='w')
+            self.imageDateLabel = ttk.Label(self.frameDetails, text=date.today().strftime("%m/%d/%y"))
+            self.imageDateLabel.grid(row=2, column=2, padx=5, pady=5, sticky='w')
+            self.outDate = date.today().strftime("%m/%d/%y")
+            # time
+            self.imageTime = ttk.Label(self.frameDetails, text="Time:")
+            self.imageTime.grid(row=3, column=1, padx=5, sticky='w')
+            self.imageTimeLabel = ttk.Label(self.frameDetails, text=datetime.now().strftime("%H:%M:%S"))
+            self.imageTimeLabel.grid(row=3, column=2, padx=5, pady=5, sticky='w')
+            self.outTime = datetime.now().strftime("%H:%M:%S")
+            # description
+            self.imageDescription = ttk.Label(self.frameDetails, text="Description:")
+            self.imageDescription.grid(row=4, column=1, padx=5, sticky='nw')
+            self.imageDescriptionInput = tk.Text(self.frameDetails, height=6, width=25)
+            self.imageDescriptionInput.grid(row=4, column=2, padx=5, pady=5)
+        
+            # Append new info button
+            self.appendInfo_button = ttk.Button(self.frameDetails, text='Append New Info', width=30, command=self.appendInfo_button_clicked)
+            self.appendInfo_button.grid(row=5, column=2, padx=5, pady=5)
+            # Append new info label
+            self.appendInfoLabel = ttk.Label(self.frameDetails, text=' ', wraplength=400)
+            self.appendInfoLabel.grid(row=6, column=2, padx=5, pady=5)
 
-        outName = self.imageNameInput.get(1.0, "end-1c")
-        outDate = date.today().strftime("%m/%d/%y")
-        outTime = datetime.now().strftime("%H:%M:%S")
-        outDes = self.imageDescriptionInput.get(1.0, "end-1c")
-        OutputString = "Image Name: {}\nDate: {}\nTime: {}\nDescription: {}".format(outName, outDate, outTime, outDes)
+
+
+       
+
+    def appendInfo_button_clicked(self):
+        # output txt file generation
+        self.outName = self.imageNameInput.get(1.0, "end-1c")
+        self.outDes = self.imageDescriptionInput.get(1.0, "end-1c")
+        OutputString = "Image Name: {}\nDate: {}\nTime: {}\nDescription: {}".format(self.outName, self.outDate, self.outTime, self.outDes)
         with open('detailOut.txt', 'w') as f:
             f.write(OutputString)
 
-    def checkMatch_button_clicked(self):
-        #Insert Hash Call
-        if(matchFound):
-            self.matchStatusLabel.configure(text="Match Found!")
-        else:
-            self.matchStatusLabel.configure(text="No match!")
+        self.appendInfoLabel.configure(text="Information Added!")
+        self.appendInfo_button['state'] = DISABLED
             
             
 
@@ -131,7 +136,7 @@ class View(ttk.Frame):
         
         # enable buttons
         self.hashify_button['state'] = NORMAL
-        self.checkMatch_button['state'] = NORMAL
+        
 
         # open image
         self.img = Image.open(self.fileName)
