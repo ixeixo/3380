4e38ce2d0584c04127ce3aecdc761404482845ba
GunnarO0 <69883423+GunnarO0@users.noreply.github.com>
2022-04-15 17:20:56 -0500

    Update App.py

--- GUI/MVC/App.py
+++ GUI/MVC/App.py
@@ -4,6 +4,7 @@ from tkinter import *
 from MVC.Model import Model
 from MVC.View import View
 from MVC.Controller import Controller
+from Hash import Hash
 
 
 
@@ -12,20 +13,25 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.title('Tkinter MVC Demo')
 
         # create a model
-        model = Model('hello@pythontutorial.net')
+        model = Model()
+        
 
         # create a view and place it on the root window
         view = View(self)
         view.grid(row=0, column=0, padx=10, pady=10)
 
         # create a controller
-        controller = Controller(model, view)
+        hashFile = Hash()
+        controller = Controller(model, view, hashFile)
 
         # set the controller to view
         view.set_controller(controller)
+
+        
+        hashFile.set_controller(controller)
+
 
         def returnFileName():
             return view.fileName
